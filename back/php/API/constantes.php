c879b83601ed10d320bb0010150a59663625e726
Lia-0106 <lia.arwenn106@gmail.com>
2026-06-06 12:37:44 +0200

    Ajout des commentaires + relecture HTML/CSS du front

--- back/php/API/constantes.php
+++ back/php/API/constantes.php
@@ -1,7 +1,10 @@
 <?php
 
-define('DB_USER', 'irveuser') ;
-define('DB_PASSWORD', 'irvepwd') ;
+// define('DB_USER', 'irveuser') ;
+// define('DB_PASSWORD', 'irvepwd') ;
+
+define('DB_USER', 'root') ;
+define('DB_PASSWORD', '') ;
 
 define('DB_NAME', 'irve') ;
 define('DB_SERVER', 'localhost') ;
